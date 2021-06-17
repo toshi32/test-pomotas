e13590e8e85d9b8ac9b1a589dc6d525c319dc7e8
toshi32 <viva.la.vida0522@gmail.com>
2021-06-17 15:13:50 +0900

    deviceのフラッシュメッセージの日本語化及びコントローラー作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
